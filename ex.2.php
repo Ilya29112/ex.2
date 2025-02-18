8a78b559681ac3eed6e06dd29a30982552c54b61
Ilya Alexandrov <ilyaalexandrov19900@gmail.com>
2025-02-18 14:38:09 +0300

    Работа №2 (2)

---
+++ ex.2.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "Введите число : ";
+$handle = fopen("php://stdin", "r");
+$number = fgets($handle);
+$number = (int)$number;
+
+echo "Простые числа от 1 до " . $number . ":\n";
+for ($i = 2; $i <= $number; $i++) {
+    $isPrime = true;
+
+    for ($j = 2; $j <= sqrt($i); $j++) {
+        if ($i % $j == 0) {
+            $isPrime = false;
+            break;
+        }
+    }
+
+    if ($isPrime) {
+        echo $i . "\n";
+    }
+}
