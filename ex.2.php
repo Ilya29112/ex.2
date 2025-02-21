b841c8c44bcac3c5b4569ad26b18f09b9481a0a3
Ilya29112 <ilyaalexandrov19977@gmail.com>
2025-02-21 10:33:23 +0300

    Update ex.2.php

--- ex.2.php
+++ ex.2.php
@@ -10,7 +10,7 @@ for ($i = 2; $i <= $number; $i++) {
     $isPrime = true;
 
     for ($j = 2; $j <= sqrt($i); $j++) {
-        if ($i % $j == 0) {
+        if ($i % $j === 0) {
             $isPrime = false;
             break;
         }
